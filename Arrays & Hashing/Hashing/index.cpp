adff0a1756791251f6cb99d7e994a5761cac1fc1
Amrit Raj <amritsingh5356@gmail.com>
2025-09-10 10:03:50 +0530

    update

--- Arrays & Hashing/Hashing/index.cpp
+++ Arrays & Hashing/Hashing/index.cpp
@@ -6,9 +6,17 @@ using namespace std;
 
 int main() {
     unordered_map<int, int> hashMap;
-    hashMap.insert({1, 100});
+    hashMap.insert({4, 100});
     hashMap.insert({2, 200});
+    hashMap.insert({3, 300});
 
-    cout << hashMap.find(1)->second << endl; // Outputs 100
+    cout << hashMap.find(4)->first << endl; // Outputs 4
     cout << hashMap.find(2)->second << endl; // Outputs 200
+    cout << hashMap.find(3)->second << endl; // Outputs 300
+
+    for (auto it = hashMap.begin(); it != hashMap.end(); it++) {
+        cout << it->first << " " << it->second << endl;
+    }
+
+    return 0;
 }
